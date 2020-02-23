ab47097e4281a8bcce921e7db05c2d38fa076500
Sharon Lee <sharonlee714@gmail.com>
2020-02-22 23:24:58 -0800

    add attempt to add button to decrease by pint

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -2,17 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-function Keg(props) {
-  return (
-    <div>
-      <h3>{props.name} - {props.alcoholContent}</h3>
-      <p><em>{props.brand}</em></p>
-      <p>{props.location}</p>
-      <p>${props.price} /pint</p>
-      <p>{props.pintsAvailable}</p>
-      <hr />
-    </div>
-  );
+class Keg extends React.Component { 
+  constructor(props){ 
+    super(props);
+    this.state = {
+      pints: 124
+    };
+    console.log(this.state.pints);
+    this.decreasePint = this.decreasePint.bind(this);
+  }
+
+  decreasePint(){
+    let newPints = this.state.pints -1;
+    this.setState({pints: newPints});
+  }
+
+  render(){
+    var buttonStyle = {
+      backgroundColor: 'pink',
+      color: 'grey',
+      border: '1px solid white'
+    }
+
+    return (
+      <div>
+        <h3>{props.name} - {props.alcoholContent}</h3>
+        <p><em>{props.brand}</em></p>
+        <p>{props.location}</p>
+        <p>${props.price} /pint</p>
+        <p>{props.pintsAvailable}</p>
+        <hr />
+        <button onClick={this.decreasePint}>Decrease</button>
+      </div>
+    );
+  }
 }
 
 Keg.propTypes = {
@@ -25,3 +48,10 @@ Keg.propTypes = {
 };
 
 export default Keg; 
+
+
+{/* <Button 
+className="btn btn-success" style={buttonStyle}
+onClick={() => this.setState({pints: this.state.pints += 1})}>
+Decrease Pint
+</Button> */}
